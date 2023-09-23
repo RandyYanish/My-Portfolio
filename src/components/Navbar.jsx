f52c365ab3cb609404f9128e291a976a82b49ca5
RandyYanish <rtaylorfargo@gmail.com>
2023-09-22 23:50:28 -0700

    Dark mode update and testing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import {
     FaBars,
@@ -12,123 +12,139 @@ import './Navbar.css';
 
 function Navbar() {
     const [nav, setNav] = useState(false);
-    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [theme, setTheme] = useState(null);
 
     const handleClick = () => setNav(!nav);
 
-    const toggleDarkMode = () => {
-        setIsDarkMode(!isDarkMode);
-    };
+    useEffect(() => {
+        if (window.matchMedia('(prefers-color-scheme: dark').matches) {
+            setTheme('dark');
+        } else {
+            setTheme('light');
+        }
+    }, []);
 
-    return (
-        <div className='w-full h-[80px] flex justify-evenly items-center px-4 bg-steel-dark text-grey-light'>
-            {/* Dark Mode Switch */}
-            <div className='flex px-4 text-grey-light hover:scale-150 duration-150 hover:text-yellow-light'>
-                {isDarkMode ? (
-                    <FaSun onClick={toggleDarkMode} className='cursor-pointer' />
-                ) : (
-                    <FaMoon onClick={toggleDarkMode} className='cursor-pointer'/>
-                )}
-            </div>
-            {/* menu */}
-            <ul className='hidden md:flex gap-x-8'>
-                <li className='nav-list-item'>
-                    <NavLink to='/'>
-                        Home
-                    </NavLink>
-                </li>
-                <li className='nav-list-item'>
-                    <NavLink to='/about'>
-                        About
-                    </NavLink>
-                </li>
-                <li className='nav-list-item'>
-                    <NavLink to='/skills'>
-                        Skills
-                    </NavLink>
-                </li>
-                <li className='nav-list-item'>
-                    <NavLink to='/works'>
-                        Work
-                    </NavLink>
-                </li>
-                <li className='nav-list-item'>
-                    <NavLink to='/contact'>
-                        Contact
-                    </NavLink>
-                </li>
-            </ul>
-            {/* Empty Spacer */}
-            <div className='flex px-4 text-grey-light'></div>
-            {/* Hamburger */}
-            <div onClick={handleClick} className='md:hidden z-10'>
-                {!nav ? <FaBars /> : <FaTimes />}
-            </div>
-            {/* Mobile menu */}
-            <ul
-                className={
-                    !nav
-                        ? 'hidden'
-                        : 'absolute top-0 left-0 w-full h-screen bg-steel-dark text-grey-light flex flex-col justify-center items-center p-4'
-                }
-            >
-                <li className='py-6 text-2xl'>
-                    <NavLink onClick={handleClick} to='/'>
-                        Home
-                    </NavLink>
-                </li>
-                <li className='py-6 text-2xl'>
-                    {' '}
-                    <NavLink onClick={handleClick} to='/about'>
-                        About
-                    </NavLink>
-                </li>
-                <li className='py-6 text-2xl'>
-                    {' '}
-                    <NavLink onClick={handleClick} to='/skills'>
-                        Skills
-                    </NavLink>
-                </li>
-                <li className='py-6 text-2xl'>
-                    {' '}
-                    <NavLink onClick={handleClick} to='/projects'>
-                        Projects
-                    </NavLink>
-                </li>
-                <li className='py-6 text-2xl'>
-                    {' '}
-                    <NavLink onClick={handleClick} to='/contact'>
-                        Contact
-                    </NavLink>
-                </li>
-            </ul>
-            {/* Social icons */}
-            <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
-                <ul>
-                    <li className='w-[150px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[0px] duration-300 bg-blue-600'>
-                        <a
-                            className='flex justify-between items-center w-full text-gray-300'
-                            href='https://www.linkedin.com/in/randalltaylor98/'
-                            target='_blank' // Open in a new tab
-                            rel='noopener noreferrer' // Security best practice
-                        >
-                            Linkedin<FaLinkedin size={40} />
-                        </a>
-                    </li>
-                    <li className='w-[150px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[0px] duration-300 bg-[#333333]'>
-                        <a
-                            className='flex justify-between items-center w-full text-gray-300'
-                            href='https://github.com/randyyanish'
-                            target='_blank' // Open in a new tab
-                            rel='noopener noreferrer' // Security best practice
-                        >
-                            Github<FaGithub size={40} />
-                        </a>
-                    </li>
-                </ul>
-            </div>
+    useEffect(() => {
+        if (theme === 'dark') {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    }, [theme]);
+
+    const handleThemeSwitch = () => {
+        setTheme(theme === 'dark' ? 'light' : 'dark');
+    }
+
+return (
+    <div className='w-full h-[80px] flex justify-evenly items-center px-4 bg-steel-dark text-grey-light dark:bg-ice-dark'>
+        {/* Dark Mode Switch */}
+        <div className='flex px-4 text-grey-light hover:scale-150 duration-150 hover:text-yellow-light'>
+            {theme === 'light' ? (
+                <FaSun onClick={handleThemeSwitch} className='cursor-pointer' size={30}/>
+            ) : (
+                <FaMoon onClick={handleThemeSwitch} className='cursor-pointer' size={30}/>
+            )}
         </div>
-    );
+        {/* menu */}
+        <ul className='hidden md:flex gap-x-8'>
+            <li className='nav-list-item'>
+                <NavLink to='/'>
+                    Home
+                </NavLink>
+            </li>
+            <li className='nav-list-item'>
+                <NavLink to='/about'>
+                    About
+                </NavLink>
+            </li>
+            <li className='nav-list-item'>
+                <NavLink to='/skills'>
+                    Skills
+                </NavLink>
+            </li>
+            <li className='nav-list-item'>
+                <NavLink to='/works'>
+                    Work
+                </NavLink>
+            </li>
+            <li className='nav-list-item'>
+                <NavLink to='/contact'>
+                    Contact
+                </NavLink>
+            </li>
+        </ul>
+        {/* Empty Spacer */}
+        <div className='flex px-4 text-grey-light'></div>
+        {/* Hamburger */}
+        <div onClick={handleClick} className='md:hidden z-10'>
+            {!nav ? <FaBars /> : <FaTimes />}
+        </div>
+        {/* Mobile menu */}
+        <ul
+            className={
+                !nav
+                    ? 'hidden'
+                    : 'absolute top-0 left-0 w-full h-screen bg-steel-dark text-grey-light flex flex-col justify-center items-center p-4'
+            }
+        >
+            <li className='py-6 text-2xl'>
+                <NavLink onClick={handleClick} to='/'>
+                    Home
+                </NavLink>
+            </li>
+            <li className='py-6 text-2xl'>
+                {' '}
+                <NavLink onClick={handleClick} to='/about'>
+                    About
+                </NavLink>
+            </li>
+            <li className='py-6 text-2xl'>
+                {' '}
+                <NavLink onClick={handleClick} to='/skills'>
+                    Skills
+                </NavLink>
+            </li>
+            <li className='py-6 text-2xl'>
+                {' '}
+                <NavLink onClick={handleClick} to='/projects'>
+                    Projects
+                </NavLink>
+            </li>
+            <li className='py-6 text-2xl'>
+                {' '}
+                <NavLink onClick={handleClick} to='/contact'>
+                    Contact
+                </NavLink>
+            </li>
+        </ul>
+        {/* Social icons */}
+        <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+            <ul>
+                <li className='w-[150px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[0px] duration-300 bg-blue-600'>
+                    <a
+                        className='flex justify-between items-center w-full text-gray-300'
+                        href='https://www.linkedin.com/in/randalltaylor98/'
+                        target='_blank' // Open in a new tab
+                        rel='noopener noreferrer' // Security best practice
+                    >
+                        Linkedin<FaLinkedin size={40} />
+                    </a>
+                </li>
+                <li className='w-[150px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[0px] duration-300 bg-[#333333]'>
+                    <a
+                        className='flex justify-between items-center w-full text-gray-300'
+                        href='https://github.com/randyyanish'
+                        target='_blank' // Open in a new tab
+                        rel='noopener noreferrer' // Security best practice
+                    >
+                        Github<FaGithub size={40} />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+);
 };
 
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx,html}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
