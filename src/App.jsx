86de72b2bfa6ddfd22e97c5ba1f558982f2552c1
RandyYanish <rtaylorfargo@gmail.com>
2023-09-30 23:57:44 -0700

    working radial waves on click

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,24 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer';
 import CursorRadialGradient from './components/CursorRadialGradient.jsx';
+import RadialWaves from './components/RadialWaves.jsx';
 
 function App() {
   return (
     <div className='flex flex-col min-h-screen bg-gradient-to-b from-steel-light dark:from-gray-950 dark:to-slate-900 duration-500 z-0'>
         <CursorRadialGradient />
+        <div className='z-1'>
+          <RadialWaves />
+        </div>
         <nav className='p-4 z-9'>
           <Navbar />
         </nav>
-        <div className='flex-grow p-4 z-1'> {/* Container for Outlet content */}
-          <div className='overflow-y-auto max-h-full'>
+        <div className='flex-grow p-4 pointer-events-none'> {/* Container for Outlet content */}
+          <div className='overflow-y-auto max-h-full pointer-events-none'>
             <Outlet /> {/* Renders child routes */}
           </div>
         </div>
-        <footer className='p-4 z-1 scale-100'>
+        <footer className='p-4 z-9 scale-100'>
           <Footer />
         </footer>
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,27 +14,27 @@ function Hero() {
         <div id='Hero' className='lg:px-24 px-16 h-auto'>
             <div className='text-left flex flex-col md:flex-row md:items-center md:justify-center'>
                 <div className='md:w-1/2 xl:w-1/3'>
-                    <h1 className='font-serif text-metal-dark dark:text-purple-light text-4xl duration-500 lg:text-5xl'>
+                    <h1 className='font-serif text-metal-dark dark:text-purple-light text-4xl duration-500 lg:text-5xl pointer-events-auto'>
                         Randall Taylor
                     </h1>
-                    <p className='typewriter anim-typewriter text-ice-dark dark:text-ice'>
+                    <p className='typewriter anim-typewriter text-ice-dark dark:text-ice pointer-events-auto'>
                         Full-Stack Dev.
                     </p>
                     <div className='hidden md:flex justify-center items-center mt-8'>
                         <button
                             onClick={handleClick}
-                            className='explore-btn rounded-full hover:bg-ice-light hover:text-magenta-dark bg-ice-dark py-2 px-8 text-steel-light text-3xl font-serif dark:hover:bg-magenta-dark dark:hover:text-ice-light dark:hover:after:border-yellow-dark'>
+                            className='explore-btn rounded-full hover:bg-ice-light hover:text-magenta-dark bg-ice-dark py-2 px-8 text-steel-light text-3xl font-serif dark:hover:bg-magenta-dark dark:hover:text-ice-light dark:hover:after:border-yellow-dark pointer-events-auto'>
                             Explore
                         </button>
                     </div>
                 </div>
                 <div className='md:w-1/2 xl:w-1/3'>
-                    <img className='rounded-3xl w-auto my-4' src={heroImage} alt='A headshot of Randall Taylor, developer.' />
+                    <img className='rounded-3xl w-auto my-4 pointer-events-auto' src={heroImage} alt='A headshot of Randall Taylor, developer.' />
                 </div>
                 <div className='flex justify-center md:hidden'>
                     <button
                         onClick={handleClick}
-                        className='explore-btn rounded-full hover:bg-steel-dark bg-ice-dark py-2 px-8 text-steel-light text-3xl font-serif'>
+                        className='explore-btn rounded-full hover:bg-steel-dark bg-ice-dark py-2 px-8 text-steel-light text-3xl font-serif pointer-events-auto'>
                         Explore
                     </button>
                 </div>
