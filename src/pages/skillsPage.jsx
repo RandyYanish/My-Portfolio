be6515eb1b8caa9a6b085c02de19e9150b9b8342
RandyYanish <rtaylorfargo@gmail.com>
2023-10-04 16:04:02 -0700

    skills page update

--- src/pages/skillsPage.jsx
+++ src/pages/skillsPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import CVButton from '../components/CV-Download';
+import Resume from '../components/Resume';
 
 function SkillsPage() {
   return (
     <div>
+      <Resume />
       <CVButton />
     </div>
   );
