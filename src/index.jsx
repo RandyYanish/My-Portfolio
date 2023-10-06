12b8dcf872b1d61dc5d2cc8b62bb53727be6456b
RandyYanish <rtaylorfargo@gmail.com>
2023-10-05 19:11:13 -0700

    routing update

--- src/index.jsx
+++ src/index.jsx
@@ -1,41 +1,21 @@
 // packages
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 // pages
 import HomePage from './pages/homePage';
-import AboutPage from './pages/aboutPage';
-import SkillsPage from './pages/skillsPage';
-import ProjectsPage from './pages/projectsPage';
-import ContactPage from './pages/contactPage';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
-    path: '/',
+    path: '/*',
     element: <App />,
     children: [
       {
-        path: '',
+        path: '/*',
         element: <HomePage />,
-      },
-      {
-        path: 'about',
-        element: <AboutPage />,
-      },
-      {
-        path: 'skills',
-        element: <SkillsPage />,
-      },
-      {
-        path: 'projects',
-        element: <ProjectsPage />,
-      },
-      {
-        path: 'contact',
-        element: <ContactPage />,
       },
     ],
   },
